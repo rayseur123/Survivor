81461e215b2a3061c841667cb2bec65b902992e3
rayseur123 <nicolas.jooris@hotmail.fr>
2026-05-28 14:44:35 +0200

    feat: add player base movement

--- src/entities/player/player.gd
+++ src/entities/player/player.gd
@@ -16,7 +16,15 @@ func _handle_player_deplacement(delta: float) -> void:
 	forward.y = 0
 	right.y = 0
 	
-	
-	print("forward : ", forward)
-	print("right : ", right)
+	var direction = Vector3.ZERO
+	if (Input.is_action_pressed("move_forward")):
+		direction += forward
+	if (Input.is_action_pressed("move_backward")):
+		direction += -forward
+	if (Input.is_action_pressed("move_right")):
+		direction += right
+	if (Input.is_action_pressed("move_left")):
+		direction += -right
+	velocity = direction.normalized() * SPEED
+	move_and_slide()
 	pass
