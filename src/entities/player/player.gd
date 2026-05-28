668f549ce7efc965c7d7f5fa13e7a338c492fa39
rayseur123 <nicolas.jooris@hotmail.fr>
2026-05-28 17:09:09 +0200

    feat: add jump player

--- src/entities/player/player.gd
+++ src/entities/player/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+@export var speed = 5.0
+@export var jump_speed = 5.0
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 @onready var camera_node = $Camera/SpringArm3D/Camera3D
 
@@ -8,8 +11,13 @@ func _physics_process(delta: float) -> void:
 		_handle_player_action(delta)
 
 func _handle_player_action(delta: float) -> void:
+	_handle_player_gravity(delta)
 	_handle_player_deplacement(delta)
-	
+
+func _handle_player_gravity(delta: float) -> void:
+	if (not is_on_floor()):
+		velocity.y -= gravity * delta
+
 func _handle_player_deplacement(delta: float) -> void:
 	var forward = -camera_node.global_transform.basis.z
 	var right = camera_node.global_transform.basis.x
@@ -25,6 +33,11 @@ func _handle_player_deplacement(delta: float) -> void:
 		direction += right
 	if (Input.is_action_pressed("move_left")):
 		direction += -right
-	velocity = direction.normalized() * SPEED
+	direction = direction.normalized()
+	
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	
+	if (Input.is_action_just_pressed("jump") and is_on_floor()):
+		velocity.y = jump_speed
 	move_and_slide()
-	pass
