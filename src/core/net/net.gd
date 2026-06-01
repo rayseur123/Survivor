af05f60f06918476f103244f88b52d33deca2cc7
Nicolas <Nicolas.jooris@hotmail.fr>
2026-06-01 23:28:07 +0200

    feat: add base coop

--- src/core/net/net.gd
+++ src/core/net/net.gd
@@ -1,33 +1,69 @@
 extends Control
 
 @export var IP_ADDRESS = "127.0.0.1"
-@export var PORT = 4242
+@export var PORT = 4245
+@export var map: PackedScene
+@export var player: PackedScene
+
+var current_map
 
 var id = 0
 
 func _ready() -> void:
-	id = multiplayer.get_unique_id()
-	multiplayer.peer_connected.connect(_on_peer_connected)
+	multiplayer.peer_connected.connect(_on_player_connected)
+	multiplayer.peer_disconnected.connect(_on_player_disconnected)
+	multiplayer.connected_to_server.connect(_on_connected_ok)
+	multiplayer.connection_failed.connect(_on_connected_fail)
+	multiplayer.server_disconnected.connect(_on_server_disconnected)
 
 func createClient() -> void:
 	var peer = ENetMultiplayerPeer.new()
 	peer.create_client(IP_ADDRESS, PORT)
 	multiplayer.multiplayer_peer = peer
-
+	id = multiplayer.get_unique_id()
 
 func createServer() -> void:
 	var peer = ENetMultiplayerPeer.new()
 	peer.create_server(PORT)
 	multiplayer.multiplayer_peer = peer	
+	id = multiplayer.get_unique_id()
+	load_map()
 
-func _on_peer_connected(id_client) -> void:
+func load_map() -> void:
+	current_map = map.instantiate()
+	current_map.name = "defaultMap"
+	add_child(current_map)
+	
+	var server_player = player.instantiate()
+	server_player.name = "1"
+	server_player.set_multiplayer_authority(1)
+	current_map.add_child(server_player)
+
+func _on_player_connected(id_client) -> void:
+	if (multiplayer.is_server()):
+		var client_player = player.instantiate()
+		client_player.name = str(id_client)
+		client_player.set_multiplayer_authority(id_client)
+		current_map.add_child(client_player)
+		
+
+func _on_connected_ok() -> void:
+	print("Connection ok")
+	
+func _on_connected_fail() -> void:
+	print("Connection fail")
+
+func _on_server_disconnected() -> void:
+	print("Server leave")
+
+func _on_player_disconnected(id_client) -> void:
 	if multiplayer.is_server():
-		print("NEW CLIENT : ", id_client)
-
+		print("THE CLIENT : ", id_client, " LEAVES")
 
 func _on_create_client_pressed() -> void:
 	createClient()
-
+	hide()
 
 func _on_create_server_pressed() -> void:
 	createServer()
+	hide()
--- src/entities/player/player.gd
+++ src/entities/player/player.gd
@@ -5,20 +5,36 @@ extends CharacterBody3D
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-@onready var camera_node = $Camera/SpringArm3D/Camera3D
+var camera_node
+
+func _enter_tree() -> void:
+	set_multiplayer_authority(str(name).to_int())
+
+func _ready() -> void:
+	camera_node = $Camera/SpringArm3D/Camera3D
+	if not is_multiplayer_authority():
+		camera_node.current = false 
+		return	
+	camera_node.current = true
 
 func _physics_process(delta: float) -> void:
-		_handle_player_action(delta)
+	if not is_multiplayer_authority():
+		return
+	_handle_player_action(delta)
 
 func _handle_player_action(delta: float) -> void:
 	_handle_player_gravity(delta)
 	_handle_player_deplacement()
+	move_and_slide()
 
 func _handle_player_gravity(delta: float) -> void:
 	if (not is_on_floor()):
 		velocity.y -= gravity * delta
 
 func _handle_player_deplacement() -> void:
+	if not camera_node:
+		return
+	
 	var forward = -camera_node.global_transform.basis.z
 	var right = camera_node.global_transform.basis.x
 	forward.y = 0
@@ -33,11 +49,12 @@ func _handle_player_deplacement() -> void:
 		direction += right
 	if (Input.is_action_pressed("move_left")):
 		direction += -right
-	direction = direction.normalized()
-	
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
-	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = 0
+		velocity.z = 0
 	if (Input.is_action_just_pressed("jump") and is_on_floor()):
 		velocity.y = jump_speed
-	move_and_slide()
